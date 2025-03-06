1eda8a091eb9bf7a15fb11051c09f0e5fe8cb6ed
philtreez <philipp.treezy@gmail.com>
2025-03-06 08:04:39 +0100

    m

--- js/app.js
+++ js/app.js
@@ -25,11 +25,11 @@ const bloomPass = new THREE.UnrealBloomPass(
   new THREE.Vector2(window.innerWidth, window.innerHeight),
   1.5,   // stärkere Werte hier, da nur selektiv angewendet
   0.4,
-  0.9
+  0.85
 );
 bloomPass.threshold = 0;
 bloomPass.strength = 1.5;
-bloomPass.radius = 0.4;
+bloomPass.radius = 0.55;
 
 const bloomComposer = new THREE.EffectComposer(renderer);
 bloomComposer.renderToScreen = false;
